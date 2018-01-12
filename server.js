b567e97211dbcfbfb23518e01b8f9ed79cc475be
Karol <karol.gratus@gmail.com>
2018-01-12 11:26:06 +0100

    first commit on passport branch

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
 const app = express();
+const googleProfile = {};
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+// app.set('view engine', 'pug');
+// app.set('views', './views');
 
-app.get('/', (req, res) => res.render('main'));
+// app.get('/', (req, res) => res.render('main'));
 
-app.get('/auth/google', (req, res) => res.render('auth'));
+// app.get('/auth/google', (req, res) => res.render('auth'));
 
-app.get('/auth/google/user', (req, res) => {
-  res.render('verify', {
-    user: {
-      email: req.query.email,
-      password: req.query.password
-    }
-  });
-});
+// app.get('/auth/google/user', (req, res) => {
+//   res.render('verify', {
+//     user: {
+//       email: req.query.email,
+//       password: req.query.password
+//     }
+//   });
+// });
 
-app.listen(3000);
+// app.listen(3000);
 
-app.use((req, res, next) => res.status(404).send('404! Page not found!'));
+// app.use((req, res, next) => res.status(404).send('404! Page not found!'));
